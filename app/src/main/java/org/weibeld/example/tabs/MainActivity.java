c7d4c6521ef48624f800005b186e32afe74d1794
Daniel Weibel <danielmweibel@gmail.com>
2016-10-23 21:35:18 +0200

    Only swipe views, no tabs
    This is always the first step to the different ways of implementing tabs
    in Android.
    
    Implementing swipe views includes the following:
    
    - ViewPager (the UI element that swipes between pages, declared in XML)
    - PagerAdapter (adaper for supplying the ViewPager with pages; pages
      typically are implemented as fragments)
    - A Fragment for each page

--- app/src/main/java/org/weibeld/example/tabs/MainActivity.java
+++ app/src/main/java/org/weibeld/example/tabs/MainActivity.java
@@ -4,9 +4,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 import org.weibeld.example.R;
@@ -14,13 +12,6 @@ import org.weibeld.example.R;
 public class MainActivity extends AppCompatActivity {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
-
-    // Titles of the individual pages (displayed in tabs)
-    private final String[] PAGE_TITLES = new String[] {
-            "Page 1",
-            "Page 2",
-            "Page 3"
-    };
 
     // The fragments that are used as the individual pages
     private final Fragment[] PAGES = new Fragment[] {
@@ -41,40 +32,6 @@ public class MainActivity extends AppCompatActivity {
         // (fragments) to the ViewPager, which the ViewPager needs to display.
         mViewPager = (ViewPager) findViewById(R.id.viewpager);
         mViewPager.setAdapter(new MyPagerAdapter(getFragmentManager()));
-
-        // Set up ActionBar tabs
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-
-            @Override
-            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-                // Link from tabs to ViewPager: when a tab is clicked, set ViewPager to this page
-                mViewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-            }
-
-            @Override
-            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-            }
-        };
-        // Add tabs to ActionBar and equip them with the TabListener defined above
-        for (int i = 0; i < PAGES.length; i++) {
-            actionBar.addTab(actionBar.newTab().setText(PAGE_TITLES[i]).setTabListener(tabListener));
-        }
-
-        // Link from ViewPager to tabs: when a new page is selected, set the active tab to this page
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
-            @Override
-            public void onPageSelected(int position) {
-                getSupportActionBar().setSelectedNavigationItem(position);
-            }
-        });
     }
 
 
@@ -95,9 +52,5 @@ public class MainActivity extends AppCompatActivity {
             return PAGES.length;
         }
 
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return PAGE_TITLES[position];
-        }
     }
 }
