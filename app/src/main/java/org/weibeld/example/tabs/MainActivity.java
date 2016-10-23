49b11a7077e45fde2803ec5cc6be59f41fc93cb5
Daniel Weibel <danielmweibel@gmail.com>
2016-10-24 00:30:58 +0200

    Scrollable TabLayout tabs
    This is the same as 06_tablayout_tabs_fixed_and_toolbar, with the difference
    that the TabLayout tab bar is scrollable rather than fixed.
    
    To get scrollable TabLayout tabs, simply do:
    
    - app:tabMode="scrollable" in <TabLayout> XML element
     (or alternatively calling tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE))

--- app/src/main/java/org/weibeld/example/tabs/MainActivity.java
+++ app/src/main/java/org/weibeld/example/tabs/MainActivity.java
@@ -3,6 +3,7 @@ package org.weibeld.example.tabs;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -16,9 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     // Titles of the individual pages (displayed in tabs)
     private final String[] PAGE_TITLES = new String[] {
-            "Page 1",
-            "Page 2",
-            "Page 3"
+            "Page 1 very very long",
+            "Page 2 very very long",
+            "Page 3 very very long"
     };
 
     // The fragments that are used as the individual pages
@@ -44,6 +45,12 @@ public class MainActivity extends AppCompatActivity {
         // (fragments) to the ViewPager, which the ViewPager needs to display.
         mViewPager = (ViewPager) findViewById(R.id.viewpager);
         mViewPager.setAdapter(new MyPagerAdapter(getFragmentManager()));
+
+        // Connect the tabs with the ViewPager (the setupWithViewPager method does this for us in
+        // both directions, i.e. when a new tab is selected, the ViewPager switches to this page,
+        // and when the ViewPager switches to a new page, the corresponding tab is selected)
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(mViewPager);
     }
 
 
